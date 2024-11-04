452210368af0e1bae2df3860b717d82951d1c782
BASIM THANI <mjnoonha90@gmail.com>
2024-11-04 17:07:56 +0300

    update 5

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,24 @@
-import ReactDOM from 'react-dom/client'
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
-import ReactQueryProvider from './providers/ReactQeuryProvider';
-import { Suspense } from 'react';
-import Spinner from './components/spinner/Spinner';
+import { Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/authContext'
+import ReactQueryProvider from './providers/ReactQeuryProvider'
+import { CartProvider } from './context/CartContext'
 import { Toaster } from 'sonner'
-import { AuthContextProvider } from './context/authContext';
+import Spinner from './components/spinner/Spinner'
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename='/'>
-    <AuthContextProvider>
-      <ReactQueryProvider>
-        <CartProvider>
-          <Suspense fallback={<Spinner/>}>
-            <Toaster/>
-            <App />
-          </Suspense>
-        </CartProvider>
-      </ReactQueryProvider>
-    </AuthContextProvider>
-  </BrowserRouter>
+  <AuthContextProvider>
+    <ReactQueryProvider>
+      <CartProvider>
+        <Suspense fallback={<Spinner/>}>
+          <Toaster/>
+          <App />
+        </Suspense>
+      </CartProvider>
+    </ReactQueryProvider>
+  </AuthContextProvider>
+</BrowserRouter>
 )
