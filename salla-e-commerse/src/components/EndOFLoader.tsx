6f5fea4f073e92a4f5bfe4aabdb1deb3a82152ed
BASIM THANI <mjnoonha90@gmail.com>
2024-11-04 09:01:31 +0300

    Remove all files from the repository

--- salla-e-commerse/src/components/EndOFLoader.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function EndOFLoader() {
-  return (
-    <div className="flex items-center justify-center">
-      <p className="py-10 text-slate-500">لا توجد منتجات أخرى للتحميل</p>
-    </div>
-  )
-}
