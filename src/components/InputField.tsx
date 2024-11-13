9e3b5173ed310bdd9ebd2e38426ade544831affe
BASIM THANI <mjnoonha90@gmail.com>
2024-11-13 11:17:54 +0300

    component final 8

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ErrorMsg from './ErrorMsg';
 
 interface InputFieldProps {
   type?: string;
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import Footer from '../components/Footer'
 import Header from '../components/Header';
 import { useContext } from 'react';
